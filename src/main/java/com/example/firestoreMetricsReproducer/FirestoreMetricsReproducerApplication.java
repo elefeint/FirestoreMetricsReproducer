49dd002dd5c8efb49e897ac9bde417c147af94d2
Elena Felder <41136058+elefeint@users.noreply.github.com>
2022-08-09 09:42:30 -0400

    fixed version injecting meter registry

--- src/main/java/com/example/firestoreMetricsReproducer/FirestoreMetricsReproducerApplication.java
+++ src/main/java/com/example/firestoreMetricsReproducer/FirestoreMetricsReproducerApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FirestoreMetricsReproducerApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(FirestoreMetricsReproducerApplication.class, args);
+        System.out.println("Enter any key to shutdown ...");
+        System.in.read();
     }
 
 }
--- src/main/java/com/example/firestoreMetricsReproducer/metrics/ReproducerMeterBinder.java
+++ src/main/java/com/example/firestoreMetricsReproducer/metrics/ReproducerMeterBinder.java
@@ -3,6 +3,9 @@ package com.example.firestoreMetricsReproducer.metrics;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.MeterBinder;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,3 +23,18 @@ public class ReproducerMeterBinder implements MeterBinder {
                 .register(registry);
     }
 }
+
+/*
+
+@Component
+public class ReproducerMeterBinder {
+
+    private final MeterService meterService;
+
+    public ReproducerMeterBinder(MeterRegistry registry, MeterService meterService) {
+        this.meterService = meterService;
+        Gauge.builder("test", meterService::count)
+                .register(registry);
+    }
+}
+ */
